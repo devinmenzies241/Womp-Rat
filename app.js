d2b13809ea4d7626430d8bfb3c376c848c4d4efa
Womp Rat <devinmenzies@gmail.com>
2023-01-04 21:12:58 -0700

    Fix Mailchimp logic for success and failure pages

--- app.js
+++ app.js
@@ -54,40 +54,51 @@ app.get("/contact", function(req, res) {
 
 //post route sends email entered to mailchimp servers
 app.post("/", (req, res) => {
+  //create variable from user input email field
   const email = req.body.email;
 
   //creates object to communicate with mailchimp api user input fields
   const data = {
     members: [{
       email_address: email,
-      status: "subscribed"
+      status: "subscribed",
     }]
   };
 
-  //stringify json data to send to mailchimp
+  //stringify json data from data object to send to mailchimp
   const jsonData = JSON.stringify(data);
   //url to post our https request to via mailchimp
   const url = `https://us12.api.mailchimp.com/3.0/lists/${listId}`;
   // options needed to authenticate Mailchimp post request to the correct API Key & Server location
   const options = {
     method: "POST",
-    auth: apiKey
+    auth: apiKey,
   };
 
-  //save request to variable
-  const request = https.request(url, options, function(response) {
-    if (response.statusCode === 200) {
-      res.sendFile(__dirname + "/success.html");
-    } else {
-      res.sendFile(__dirname + "/failure.html");
-    }
+  //save request to variable & process logic for redirect pages. 
+  const request = https.request(url, options, function(response) { 
+    // opon receiving the JSON response from Mailchimp's api, parse that data to look for errors
     response.on("data", function(data) {
-      console.log(JSON.parse(data));
+      const responseBack = JSON.parse(data);
+      if (responseBack.errors[0]) { //if errors are present 
+        console.log(responseBack.errors[0]);//log the errors 
+        //if we receive the specific error that a contact already exists, send the contact exists failure page
+        if(responseBack.errors[0].error_code === "ERROR_CONTACT_EXISTS") {
+          res.sendFile(__dirname + "/failureContactExists.html"); 
+        //otherwise, just send the normal failure page for all other errors
+        } else {
+          res.sendFile(__dirname + "/failure.html");
+        }
+      //if no errors are found, send the success page
+      } else {
+        console.log("No errors, contact added!");
+        res.sendFile(__dirname + "/success.html");
+      }
     });
   });
-  //send our stringified JSON to mailchimp
+  //send our stringified JSON to mailchimp to post to their server
   request.write(jsonData);
-  //end the request & stop page loading
+  //end the request & stop page from infinitely looping
   request.end();
 });
 
@@ -102,41 +113,46 @@ app.post("/failure", function(req, res) {
   res.redirect("/");
 });
 
-// Contact page form requests
+// Contact page form requests, same process as the post request for the home page.
 app.post("/contact", (req, res) => {
   const email = req.body.email;
 
-  //creates object to communicate with mailchimp api user input fields
   const data = {
     members: [{
       email_address: email,
       status: "subscribed"
     }]
   };
+
   const jsonData = JSON.stringify(data);
+
   const url = `https://us12.api.mailchimp.com/3.0/lists/${listId}`;
+
   const options = {
     method: "POST",
     auth: apiKey
   };
+
+  //same process as above to redirect user to correct success vs. failure pages
   const request = https.request(url, options, function(response) {
-    if (response.statusCode === 200) {
-      res.sendFile(__dirname + "/success.html");
-    } else {
-      res.sendFile(__dirname + "/failure.html");
-    }
     response.on("data", function(data) {
-      console.log(JSON.parse(data));
+      const responseBack = JSON.parse(data);
+      if (responseBack.errors[0]) {
+        console.log(responseBack.errors[0]);
+        if(responseBack.errors[0].error_code === "ERROR_CONTACT_EXISTS") {
+          res.sendFile(__dirname + "/failureContactExists.html"); 
+        } else {
+          res.sendFile(__dirname + "/failure.html");
+        }
+      } else {
+        res.sendFile(__dirname + "/success.html");
+      }
     });
   });
   request.write(jsonData);
   request.end();
 });
 
-//starts server on heroku / localhost:3000
+//starts server on PORT / localhost:3000
 app.listen(process.env.PORT || 3000)
 console.log("Server Started!")
-
-
-
-
